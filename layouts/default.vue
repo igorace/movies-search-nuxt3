1fac48ad0d04162547ef51a6a6eab6197bd8965d
rogisa <igor9669@gmail.com>
2023-02-15 10:04:16 +0100

    playing with rendering modes

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,10 @@ const user = useUser();
     <NuxtLink to="/movies">Movies</NuxtLink>
     <NuxtLink v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
     <a v-else href="#" @click.prevent="user.isLoggedIn = false">Log out</a>
+    <NuxtLink to="/swr">SWR</NuxtLink>
+    <NuxtLink to="/spa">SPA</NuxtLink>
+    <NuxtLink to="/static">Static</NuxtLink>
+    <NuxtLink to="/ssr">SSR</NuxtLink>
   </nav>
   <slot></slot>
 </template>
